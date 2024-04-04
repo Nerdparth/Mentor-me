b20aea9da88e3a533ef5c9417592f3e53678675f
Dilpreet010 <dilpreetkaur7075@gmail.com>
2024-04-04 17:42:05 +0530

    navbar and scroller commited

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ const menuToggle = document.querySelector('.menu-toggle');
         cursor.style.left = event.clientX + "px";
         cursor.style.top = event.clientY + "px";
     });
+   
+ // whenever there is a scroll on the body the navbar appearence will change
+    gsap.to("nav",{
+        backgroundColor :"#000",
+        height:"90px",
+        duration:0.5,
+        scrollTrigger:{
+            trigger:"#nav",
+            scroller:"body",
+            // markers:true,
+            start:"top -10%",
+            end:"top -11%",
+            scrub:1
+    
+        }
+    })
